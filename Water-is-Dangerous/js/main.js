4d2d0958a84cd8febe30517be28a55b077029f69
yathish1618 <yathish.iitkgp@gmail.com>
2016-03-30 10:11:17 +0530

    Added files via upload

---
+++ Water-is-Dangerous/js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+  
+  // variables
+  var $header_top = $('.header-top');
+  var $nav = $('nav');
+
+
+
+  // toggle menu 
+  $header_top.find('a').on('click', function() {
+    $(this).parent().toggleClass('open-menu');
+  });
+
+
+
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionsColor: ['#D85204', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#D8B26A', '#FBE42D', '#D88C19', '#10B1C8'],
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false,
+    anchors: ['begin', '01', '02', '03', '04', '05', '06', '07', 'end'],
+    menu: '#menu',
+
+    afterLoad: function(anchorLink, index) {
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+      if (index == 9) {
+          $('#fp-nav').hide();
+        }
+    },
+
+    onLeave: function(index, nextIndex, direction) {
+      if(index == 9) {
+        $('#fp-nav').show();
+      }
+    },
+
+    afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+      if(anchorLink == 'ninthSection' && slideIndex == 1) {
+        $.fn.fullpage.setAllowScrolling(false, 'up');
+        $header_top.css('background', 'transparent');
+        $nav.css('background', 'transparent');
+        $(this).css('background', '#374140');
+        $(this).find('h2').css('color', 'white');
+        $(this).find('h3').css('color', 'white');
+        $(this).find('p').css(
+          {
+            'color': '#DC3522',
+            'opacity': 1,
+            'transform': 'translateY(0)'
+          }
+        );
+      }
+    },
+
+    onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+      if(anchorLink == 'ninthSection' && slideIndex == 1) {
+        $.fn.fullpage.setAllowScrolling(true, 'up');
+        $header_top.css('background', 'rgba(0, 47, 77, .3)');
+        $nav.css('background', 'rgba(0, 47, 77, .25)');
+      }
+    },
+
+	css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+  }); 
+});
