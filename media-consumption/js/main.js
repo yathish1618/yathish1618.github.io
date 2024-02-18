aad91c2aa6c36876fd59f1d092fc5e2c670f6ab8
yathish1618 <yathish.iitkgp@gmail.com>
2024-02-18 10:36:48 +0530

    general update

--- media-consumption/js/main.js
+++ media-consumption/js/main.js
@@ -1,5 +1,5 @@
 // global variable to maintain latest values and store full source data
-var year = 2023;
+var year = 2024;
 var media = 1,
     imdbData, movies, grData, books, gamesData, games, sort=1;
 var musicData = [];
